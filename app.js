9643761d58fd8c842422c76750042bd4ca6919e3
jafRodriguez <jafRodriguezDev@gmail.com>
2020-06-05 21:47:00 -0500

    asd

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, () => {
+app.listen(process.env.PORT, () => {
 	console.log('The YelpCamp Server Has Started!');
 });
